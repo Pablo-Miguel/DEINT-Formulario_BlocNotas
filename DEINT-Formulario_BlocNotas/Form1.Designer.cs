2c2d8716abe6d9d195844b42b528d8f07b4fe806
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-11-17 14:38:33 +0100

    Agregar archivos de proyecto.

---
+++ DEINT-Formulario_BlocNotas/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace DEINT_Formulario_BlocNotas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.menuArchivo = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuArchivoNuevo = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuArchivoAbrir = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuArchivoGuardar = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuArchivoSalir = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdicion = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdicionCortar = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdicionCopiar = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdicionPegar = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdicionBuscar = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdicionFuente = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEdicionColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuImg = new System.Windows.Forms.ToolStrip();
+            this.menuImgNuevo = new System.Windows.Forms.ToolStripButton();
+            this.menuImgAbrir = new System.Windows.Forms.ToolStripButton();
+            this.menuImgGuardar = new System.Windows.Forms.ToolStripButton();
+            this.menuImgCortar = new System.Windows.Forms.ToolStripButton();
+            this.menuImgCopiar = new System.Windows.Forms.ToolStripButton();
+            this.menuImgPegar = new System.Windows.Forms.ToolStripButton();
+            this.menuImgBuscar = new System.Windows.Forms.ToolStripButton();
+            this.menuImgFuente = new System.Windows.Forms.ToolStripButton();
+            this.menuImgColor = new System.Windows.Forms.ToolStripButton();
+            this.txtNotas = new System.Windows.Forms.RichTextBox();
+            this.statusMenu = new System.Windows.Forms.StatusStrip();
+            this.menu.SuspendLayout();
+            this.menuImg.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuArchivo,
+            this.menuEdicion});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(800, 24);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            // 
+            // menuArchivo
+            // 
+            this.menuArchivo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuArchivoNuevo,
+            this.menuArchivoAbrir,
+            this.menuArchivoGuardar,
+            this.menuArchivoSalir});
+            this.menuArchivo.Name = "menuArchivo";
+            this.menuArchivo.Size = new System.Drawing.Size(60, 20);
+            this.menuArchivo.Text = "&Archivo";
+            // 
+            // menuArchivoNuevo
+            // 
+            this.menuArchivoNuevo.Name = "menuArchivoNuevo";
+            this.menuArchivoNuevo.Size = new System.Drawing.Size(180, 22);
+            this.menuArchivoNuevo.Text = "&Nuevo";
+            // 
+            // menuArchivoAbrir
+            // 
+            this.menuArchivoAbrir.Name = "menuArchivoAbrir";
+            this.menuArchivoAbrir.Size = new System.Drawing.Size(180, 22);
+            this.menuArchivoAbrir.Text = "&Abrir";
+            // 
+            // menuArchivoGuardar
+            // 
+            this.menuArchivoGuardar.Name = "menuArchivoGuardar";
+            this.menuArchivoGuardar.Size = new System.Drawing.Size(180, 22);
+            this.menuArchivoGuardar.Text = "&Guardar";
+            // 
+            // menuArchivoSalir
+            // 
+            this.menuArchivoSalir.Name = "menuArchivoSalir";
+            this.menuArchivoSalir.Size = new System.Drawing.Size(180, 22);
+            this.menuArchivoSalir.Text = "&Salir";
+            this.menuArchivoSalir.Click += new System.EventHandler(this.menuArchivoSalir_Click);
+            // 
+            // menuEdicion
+            // 
+            this.menuEdicion.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuEdicionCortar,
+            this.menuEdicionCopiar,
+            this.menuEdicionPegar,
+            this.menuEdicionBuscar,
+            this.menuEdicionFuente,
+            this.menuEdicionColor});
+            this.menuEdicion.Name = "menuEdicion";
+            this.menuEdicion.Size = new System.Drawing.Size(58, 20);
+            this.menuEdicion.Text = "&Edición";
+            // 
+            // menuEdicionCortar
+            // 
+            this.menuEdicionCortar.Name = "menuEdicionCortar";
+            this.menuEdicionCortar.Size = new System.Drawing.Size(180, 22);
+            this.menuEdicionCortar.Text = "&Cortar";
+            // 
+            // menuEdicionCopiar
+            // 
+            this.menuEdicionCopiar.Name = "menuEdicionCopiar";
+            this.menuEdicionCopiar.Size = new System.Drawing.Size(180, 22);
+            this.menuEdicionCopiar.Text = "&Copiar";
+            // 
+            // menuEdicionPegar
+            // 
+            this.menuEdicionPegar.Name = "menuEdicionPegar";
+            this.menuEdicionPegar.Size = new System.Drawing.Size(180, 22);
+            this.menuEdicionPegar.Text = "&Pegar";
+            // 
+            // menuEdicionBuscar
+            // 
+            this.menuEdicionBuscar.Name = "menuEdicionBuscar";
+            this.menuEdicionBuscar.Size = new System.Drawing.Size(180, 22);
+            this.menuEdicionBuscar.Text = "&Buscar";
+            // 
+            // menuEdicionFuente
+            // 
+            this.menuEdicionFuente.Name = "menuEdicionFuente";
+            this.menuEdicionFuente.Size = new System.Drawing.Size(180, 22);
+            this.menuEdicionFuente.Text = "&Fuente";
+            // 
+            // menuEdicionColor
+            // 
+            this.menuEdicionColor.Name = "menuEdicionColor";
+            this.menuEdicionColor.Size = new System.Drawing.Size(180, 22);
+            this.menuEdicionColor.Text = "&Color";
+            // 
+            // menuImg
+            // 
+            this.menuImg.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuImgNuevo,
+            this.menuImgAbrir,
+            this.menuImgGuardar,
+            this.menuImgCortar,
+            this.menuImgCopiar,
+            this.menuImgPegar,
+            this.menuImgBuscar,
+            this.menuImgFuente,
+            this.menuImgColor});
+            this.menuImg.Location = new System.Drawing.Point(0, 24);
+            this.menuImg.Name = "menuImg";
+            this.menuImg.Size = new System.Drawing.Size(800, 25);
+            this.menuImg.TabIndex = 1;
+            this.menuImg.Text = "toolStrip1";
+            // 
+            // menuImgNuevo
+            // 
+            this.menuImgNuevo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.menuImgNuevo.Image = ((System.Drawing.Image)(resources.GetObject("menuImgNuevo.Image")));
+            this.menuImgNuevo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuImgNuevo.Name = "menuImgNuevo";
+            this.menuImgNuevo.Size = new System.Drawing.Size(23, 22);
+            this.menuImgNuevo.Text = "toolStripButton1";
+            // 
+            // menuImgAbrir
+            // 
+            this.menuImgAbrir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.menuImgAbrir.Image = ((System.Drawing.Image)(resources.GetObject("menuImgAbrir.Image")));
+            this.menuImgAbrir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuImgAbrir.Name = "menuImgAbrir";
+            this.menuImgAbrir.Size = new System.Drawing.Size(23, 22);
+            this.menuImgAbrir.Text = "toolStripButton2";
+            // 
+            // menuImgGuardar
+            // 
+            this.menuImgGuardar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.menuImgGuardar.Image = ((System.Drawing.Image)(resources.GetObject("menuImgGuardar.Image")));
+            this.menuImgGuardar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuImgGuardar.Name = "menuImgGuardar";
+            this.menuImgGuardar.Size = new System.Drawing.Size(23, 22);
+            this.menuImgGuardar.Text = "toolStripButton3";
+            // 
+            // menuImgCortar
+            // 
+            this.menuImgCortar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.menuImgCortar.Image = ((System.Drawing.Image)(resources.GetObject("menuImgCortar.Image")));
+            this.menuImgCortar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuImgCortar.Name = "menuImgCortar";
+            this.menuImgCortar.Size = new System.Drawing.Size(23, 22);
+            this.menuImgCortar.Text = "toolStripButton4";
+            // 
+            // menuImgCopiar
+            // 
+            this.menuImgCopiar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.menuImgCopiar.Image = ((System.Drawing.Image)(resources.GetObject("menuImgCopiar.Image")));
+            this.menuImgCopiar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuImgCopiar.Name = "menuImgCopiar";
+            this.menuImgCopiar.Size = new System.Drawing.Size(23, 22);
+            this.menuImgCopiar.Text = "toolStripButton5";
+            // 
+            // menuImgPegar
+            // 
+            this.menuImgPegar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.menuImgPegar.Image = ((System.Drawing.Image)(resources.GetObject("menuImgPegar.Image")));
+            this.menuImgPegar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuImgPegar.Name = "menuImgPegar";
+            this.menuImgPegar.Size = new System.Drawing.Size(23, 22);
+            this.menuImgPegar.Text = "toolStripButton6";
+            // 
+            // menuImgBuscar
+            // 
+            this.menuImgBuscar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.menuImgBuscar.Image = ((System.Drawing.Image)(resources.GetObject("menuImgBuscar.Image")));
+            this.menuImgBuscar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuImgBuscar.Name = "menuImgBuscar";
+            this.menuImgBuscar.Size = new System.Drawing.Size(23, 22);
+            this.menuImgBuscar.Text = "toolStripButton7";
+            // 
+            // menuImgFuente
+            // 
+            this.menuImgFuente.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.menuImgFuente.Image = ((System.Drawing.Image)(resources.GetObject("menuImgFuente.Image")));
+            this.menuImgFuente.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuImgFuente.Name = "menuImgFuente";
+            this.menuImgFuente.Size = new System.Drawing.Size(23, 22);
+            this.menuImgFuente.Text = "toolStripButton8";
+            // 
+            // menuImgColor
+            // 
+            this.menuImgColor.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.menuImgColor.Image = ((System.Drawing.Image)(resources.GetObject("menuImgColor.Image")));
+            this.menuImgColor.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menuImgColor.Name = "menuImgColor";
+            this.menuImgColor.Size = new System.Drawing.Size(23, 22);
+            this.menuImgColor.Text = "toolStripButton9";
+            // 
+            // txtNotas
+            // 
+            this.txtNotas.Location = new System.Drawing.Point(0, 52);
+            this.txtNotas.Name = "txtNotas";
+            this.txtNotas.Size = new System.Drawing.Size(800, 373);
+            this.txtNotas.TabIndex = 2;
+            this.txtNotas.Text = "";
+            this.txtNotas.Visible = false;
+            // 
+            // statusMenu
+            // 
+            this.statusMenu.Location = new System.Drawing.Point(0, 428);
+            this.statusMenu.Name = "statusMenu";
+            this.statusMenu.Size = new System.Drawing.Size(800, 22);
+            this.statusMenu.TabIndex = 3;
+            this.statusMenu.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusMenu);
+            this.Controls.Add(this.txtNotas);
+            this.Controls.Add(this.menuImg);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menu;
+            this.Name = "Form1";
+            this.Text = "Bloc de notas";
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.menuImg.ResumeLayout(false);
+            this.menuImg.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem menuArchivo;
+        private System.Windows.Forms.ToolStripMenuItem menuArchivoNuevo;
+        private System.Windows.Forms.ToolStripMenuItem menuArchivoAbrir;
+        private System.Windows.Forms.ToolStripMenuItem menuArchivoGuardar;
+        private System.Windows.Forms.ToolStripMenuItem menuArchivoSalir;
+        private System.Windows.Forms.ToolStripMenuItem menuEdicion;
+        private System.Windows.Forms.ToolStripMenuItem menuEdicionCortar;
+        private System.Windows.Forms.ToolStripMenuItem menuEdicionCopiar;
+        private System.Windows.Forms.ToolStripMenuItem menuEdicionPegar;
+        private System.Windows.Forms.ToolStripMenuItem menuEdicionBuscar;
+        private System.Windows.Forms.ToolStripMenuItem menuEdicionFuente;
+        private System.Windows.Forms.ToolStripMenuItem menuEdicionColor;
+        private System.Windows.Forms.ToolStrip menuImg;
+        private System.Windows.Forms.RichTextBox txtNotas;
+        private System.Windows.Forms.StatusStrip statusMenu;
+        private System.Windows.Forms.ToolStripButton menuImgNuevo;
+        private System.Windows.Forms.ToolStripButton menuImgAbrir;
+        private System.Windows.Forms.ToolStripButton menuImgGuardar;
+        private System.Windows.Forms.ToolStripButton menuImgCortar;
+        private System.Windows.Forms.ToolStripButton menuImgCopiar;
+        private System.Windows.Forms.ToolStripButton menuImgPegar;
+        private System.Windows.Forms.ToolStripButton menuImgBuscar;
+        private System.Windows.Forms.ToolStripButton menuImgFuente;
+        private System.Windows.Forms.ToolStripButton menuImgColor;
+    }
+}
+
---
+++ DEINT-Formulario_BlocNotas/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEINT_Formulario_BlocNotas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void menuArchivoSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
